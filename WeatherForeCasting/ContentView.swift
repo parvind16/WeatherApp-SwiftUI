3ea36cbca84f691c00a969f7cbd4872b5008be79
Parvind Bhatt <p.parvind.bhatt@paymaya.com>
2024-09-27 09:02:19 +0530

    Initial Commit

---
+++ WeatherForeCasting/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherForeCasting
+//
+//  Created by Parvind Bhatt on 27/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherForeCasting/WeatherForeCastingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForeCastingApp.swift
+//  WeatherForeCasting
+//
+//  Created by Parvind Bhatt on 27/09/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForeCastingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
